2c78a3c6bb024d1108ccb4f0ddfe9d6afbaa7e5a
Johan <johand@openmailbox.org>
2022-08-31 19:23:06 -0400

    Configuring devise
    - Adding devise-jwt configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
+  respond_to :json
+  include ActionController::MimeResponds
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
