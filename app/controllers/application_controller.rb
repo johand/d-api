2a6236fb9919300dabc6995245f79c1a749bfc03
Johan <johand@openmailbox.org>
2022-09-02 17:27:55 -0400

    Method to catch exceptions on the controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,13 @@ class ApplicationController < ActionController::API
   respond_to :json
   include ActionController::MimeResponds
   before_action :configure_permitted_parameters, if: :devise_controller?
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   protected
+
+  def record_not_found(exception)
+    render json: { error: exception.message }, status: 404
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
