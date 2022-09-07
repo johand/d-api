54af6fcef8a4b303dbef33b9ff63b6df79925202
Johan <johand@openmailbox.org>
2022-09-07 19:56:06 -0400

    Setting default format to json in devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,8 @@ Rails.application.routes.draw do
                sessions: 'users/sessions',
                registrations: 'users/registrations',
                passwords: 'users/passwords'
-             }
+             }, defaults: { format: :json }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
